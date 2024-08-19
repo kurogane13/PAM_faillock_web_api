f377a425389824c0cfe5a600acf15378a71cfe1a
Gus <akaisuzuki@gmail.com>
2024-08-19 18:16:41 -0300

    Update pam_files_backup_setup.sh

--- pam_files_backup_setup.sh
+++ pam_files_backup_setup.sh
@@ -12,10 +12,13 @@ backup_and_replace() {
     echo "Backing up /etc/pam.d/sshd to /etc/pam.d/sshd.original"
     sudo cp /etc/pam.d/sshd /etc/pam.d/sshd.original
 
+    echo "Backing up /etc/pam.d/gdm-password to /etc/pam.d/gdm-password.original"
+    sudo cp /etc/pam.d/gdm-password /etc/pam.d/gdm-password.original
+
     echo "Backing up /etc/security/faillock.conf to /etc/security/faillock.conf.original"
     sudo cp /etc/security/faillock.conf /etc/security/faillock.conf.original
 
-    # Move the new files to their destinations
+    # Copy the new files to their destinations
     echo "Copying pam_files/system-auth to /etc/pam.d/system-auth"
     sudo cp pam_files/system-auth /etc/pam.d/system-auth
 
@@ -24,6 +27,9 @@ backup_and_replace() {
 
     echo "Copying pam_files/sshd to /etc/pam.d/sshd"
     sudo cp pam_files/sshd /etc/pam.d/sshd
+
+    echo "Copying pam_files/gdm-password to /etc/pam.d/gdm-password"
+    sudo cp pam_files/gdm-password /etc/pam.d/gdm-password
 
     echo "Copying pam_files/faillock.conf to /etc/security/faillock.conf"
     sudo cp pam_files/faillock.conf /etc/security/faillock.conf
